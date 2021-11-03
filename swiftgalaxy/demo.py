30db53d75eb8ae9b3ae02012172a81085b82ad74
Kyle Oman <kyle.a.oman@durham.ac.uk>
2021-11-03 22:23:19 +0000

    Adapt paths to host in demo.

--- swiftgalaxy/demo.py
+++ swiftgalaxy/demo.py
@@ -1,10 +1,15 @@
+import socket
 import unyt as u
 from os import path
 from ._swiftgalaxy import SWIFTGalaxy
 
 snapnum = 23
-base_dir = '/home/koman/'\
-    '106e3_104b2_norm_0p3_new_cooling_L006N188/'
+if 'cosma' in socket.gethostname():
+    base_dir = '/cosma7/data/dp004/dc-chai1/HAWK/'\
+        '106e3_104b2_norm_0p3_new_cooling_L006N188/'
+elif 'autarch' in socket.gethostname():
+    base_dir = '/home/koman/'\
+        '106e3_104b2_norm_0p3_new_cooling_L006N188/'
 velociraptor_filebase = path.join(base_dir, 'halo_{:04d}'.format(snapnum))
 snapshot_filename = path.join(base_dir, 'colibre_{:04d}.hdf5'.format(snapnum))
 
