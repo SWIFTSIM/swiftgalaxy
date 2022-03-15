bbda279e1a88def025558ce2136b5e5108dddee3
Kyle Oman <kyle.a.oman@durham.ac.uk>
2022-03-15 11:56:08 +0000

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,13 +4,15 @@ setup(
     name='swiftgalaxy',
     version='0.0',
     description='Code abstraction of objects (galaxies) in simulations.',
-    url='',
+    url='https://github.com/kyleaoman/swiftgalaxy',
     author='Kyle Oman',
     author_email='kyle.a.oman@durham.ac.uk',
     license='GNU GPL v3',
     packages=['swiftgalaxy'],
-    install_requires=['numpy', 'astropy', 'h5py', 'unyt', 'swiftsimio',
-                      'velociraptor'],
+    install_requires=['numpy', 'h5py', 'unyt', 'swiftsimio'],
+    extras_require=dict(
+        velociraptor='velociraptor'
+    ),
     include_package_data=True,
     zip_safe=False
 )
