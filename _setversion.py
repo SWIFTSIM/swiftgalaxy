44f8f95e282ec4bf20c7d6d2045ac7c13f489197
Kyle Oman <kyle.a.oman@durham.ac.uk>
2023-04-12 17:17:36 +0100

    Add newline to version file for linter.

--- _setversion.py
+++ _setversion.py
@@ -7,6 +7,6 @@ version = argv[1]
 with open(
     os.path.join(os.path.dirname(__file__), "swiftgalaxy", "__version__.py"), "w"
 ) as version_file:
-    version_file.write(f'__version__ = "{version}"')
+    version_file.write(f'__version__ = "{version}"\n')
 
 gencodemeta()
