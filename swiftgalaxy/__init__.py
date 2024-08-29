f89d62bb8ef6529a192cb006f1f953cd6e45ca14
Kyle Oman <kyle.a.oman@durham.ac.uk>
2024-08-29 15:16:29 +0100

    Resolve flake8 error.

--- swiftgalaxy/__init__.py
+++ swiftgalaxy/__init__.py
@@ -6,5 +6,13 @@ from .__version__ import __version__
 name = "swiftgalaxy"
 
 __doc__ = """
-SWIFTGalaxy is a module that extends SWIFTSimIO tailored to analyses of particles belonging to individual simulated galaxies. It inherits from and extends the functionality of the SWIFTDataset. It understands the content of halo catalogues (supported: SOAP, Velociraptor, Caesar) and therefore which particles belong to a galaxy or other group of particles, and its integrated properties. The particles occupy a coordinate frame that is enforced to be consistent, such that particles loaded on-the-fly will match e.g. rotations and translations of particles already in memory. Intuitive masking of particle datasets is also enabled. Finally, some utilities to make working in cylindrical and spherical coordinate systems more convenient are also provided.
+SWIFTGalaxy is a module that extends SWIFTSimIO tailored to analyses of particles
+belonging to individual simulated galaxies. It inherits from and extends the functionality
+of the SWIFTDataset. It understands the content of halo catalogues (supported: SOAP,
+Velociraptor, Caesar) and therefore which particles belong to a galaxy or other group of
+particles, and its integrated properties. The particles occupy a coordinate frame that is
+enforced to be consistent, such that particles loaded on-the-fly will match e.g. rotations
+and translations of particles already in memory. Intuitive masking of particle datasets is
+also enabled. Finally, some utilities to make working in cylindrical and spherical
+coordinate systems more convenient are also provided.
 """
