7a8061683e923e8eb0204a7442d9107ba857ef7b
Kyle Oman <kyle.a.oman@durham.ac.uk>
2025-04-12 22:55:03 +0100

    Tests for the entire visualisation suite with recentering.

--- tests/test_visualisation.py
+++ tests/test_visualisation.py
@@ -1,43 +1,161 @@
 import pytest
 import numpy as np
 import unyt as u
-from swiftsimio import cosmo_array
-from swiftsimio.visualisation.projection import project_pixel_grid
+from swiftsimio import cosmo_array, cosmo_quantity
+from swiftsimio.visualisation.projection import project_gas
+from swiftsimio.visualisation.slice import slice_gas
+from swiftsimio.visualisation.volume_render import render_gas
+from swiftsimio.visualisation.ray_trace import panel_gas
 
 
-@pytest.mark.parametrize("z_slice", [False, True])
-def test_recentered_visualisation(sg, sg_autorecentre_off, z_slice):
-    """
-    We should be able to make the same visualisations whether we recentered or not.
-    """
-    z_limits = [1.9, 2.1] if z_slice else []
-    recentered_z_limits = [-0.1, 0.1] if z_slice else []
-    kwargs = {
-        "resolution": 4,
-        "project": "masses",
-        "parallel": True,
-        "periodic": True,
-    }
-    ref_img = project_pixel_grid(
-        sg_autorecentre_off.gas,
-        region=cosmo_array(
-            [1.5, 2.5, 1.5, 2.5] + z_limits,
-            u.Mpc,
-            comoving=True,
-            scale_factor=1,
-            scale_exponent=1,
-        ),
-        **kwargs,
-    )
-    recentered_img = project_pixel_grid(
-        sg.gas,
-        region=cosmo_array(
-            [-0.5, 0.5, -0.5, 0.5] + recentered_z_limits,
-            u.Mpc,
-            comoving=True,
-            scale_factor=1,
-            scale_exponent=1,
-        ),
-        **kwargs,
-    )
-    assert np.allclose(ref_img, recentered_img)
+class TestRecenteredVisualisation:
+
+    @pytest.mark.parametrize("z_slice", [False, True])
+    def test_recentered_projection(self, sg, sg_autorecentre_off, z_slice):
+        """
+        We should be able to make the same projections whether we recentered or not.
+        """
+        z_limits = [1.9, 2.1] if z_slice else []
+        recentered_z_limits = [-0.1, 0.1] if z_slice else []
+        kwargs = {
+            "resolution": 4,
+            "project": "masses",
+            "parallel": True,
+            "periodic": True,
+        }
+        ref_img = project_gas(
+            sg_autorecentre_off,
+            region=cosmo_array(
+                [1.5, 2.5, 1.5, 2.5] + z_limits,
+                u.Mpc,
+                comoving=True,
+                scale_factor=1,
+                scale_exponent=1,
+            ),
+            **kwargs,
+        )
+        recentered_img = project_gas(
+            sg,
+            region=cosmo_array(
+                [-0.5, 0.5, -0.5, 0.5] + recentered_z_limits,
+                u.Mpc,
+                comoving=True,
+                scale_factor=1,
+                scale_exponent=1,
+            ),
+            **kwargs,
+        )
+        assert np.allclose(ref_img, recentered_img)
+
+    def test_recentered_slice(self, sg, sg_autorecentre_off):
+        """
+        We should be able to make the same slice whether we recentered or not.
+        """
+        z_slice = cosmo_quantity(
+            1.95, u.Mpc, comoving=True, scale_factor=1, scale_exponent=1
+        )
+        recentered_z_slice = cosmo_quantity(
+            -0.05, u.Mpc, comoving=True, scale_factor=1, scale_exponent=1
+        )
+        kwargs = {
+            "resolution": 4,
+            "project": "masses",
+            "parallel": True,
+            "periodic": True,
+        }
+        ref_img = slice_gas(
+            sg_autorecentre_off,
+            z_slice=z_slice,
+            region=cosmo_array(
+                [1.5, 2.5, 1.5, 2.5],
+                u.Mpc,
+                comoving=True,
+                scale_factor=1,
+                scale_exponent=1,
+            ),
+            **kwargs,
+        )
+        recentered_img = slice_gas(
+            sg,
+            z_slice=recentered_z_slice,
+            region=cosmo_array(
+                [-0.5, 0.5, -0.5, 0.5],
+                u.Mpc,
+                comoving=True,
+                scale_factor=1,
+                scale_exponent=1,
+            ),
+            **kwargs,
+        )
+        assert np.allclose(ref_img, recentered_img)
+
+    def test_recentered_volume_render(self, sg, sg_autorecentre_off):
+        """
+        We should be able to make the same rendering whether we recentered or not.
+        """
+        kwargs = {
+            "resolution": 4,
+            "project": "masses",
+            "parallel": True,
+            "periodic": True,
+        }
+        ref_img = render_gas(
+            sg_autorecentre_off,
+            region=cosmo_array(
+                [1.5, 2.5, 1.5, 2.5, 1.5, 2.5],
+                u.Mpc,
+                comoving=True,
+                scale_factor=1,
+                scale_exponent=1,
+            ),
+            **kwargs,
+        )
+        recentered_img = render_gas(
+            sg,
+            region=cosmo_array(
+                [-0.5, 0.5, -0.5, 0.5, -0.5, 0.5],
+                u.Mpc,
+                comoving=True,
+                scale_factor=1,
+                scale_exponent=1,
+            ),
+            **kwargs,
+        )
+        assert np.allclose(ref_img, recentered_img)
+
+    @pytest.mark.skip(reason="ray trace not documented (not fully implemented?)")
+    @pytest.mark.parametrize("z_slice", [False, True])
+    def test_recentered_ray_trace(self, sg, sg_autorecentre_off, z_slice):
+        """
+        We should be able to make the same ray trace whether we recentered or not.
+        """
+        z_limits = [1.5, 2.5] if z_slice else []
+        recentered_z_limits = [-0.5, 0.5] if z_slice else []
+        kwargs = {
+            "resolution": 4,
+            "panels": 4,
+            "project": "masses",
+        }
+        ref_img = panel_gas(
+            sg_autorecentre_off,
+            region=cosmo_array(
+                [1.5, 2.5, 1.5, 2.5] + z_limits,
+                u.Mpc,
+                comoving=True,
+                scale_factor=1,
+                scale_exponent=1,
+            ),
+            **kwargs,
+        )
+        recentered_img = panel_gas(
+            sg,
+            region=cosmo_array(
+                [-0.5, 0.5, -0.5, 0.5] + recentered_z_limits,
+                u.Mpc,
+                comoving=True,
+                scale_factor=1,
+                scale_exponent=1,
+            ),
+            **kwargs,
+        )
+        assert np.allclose(ref_img, recentered_img)
