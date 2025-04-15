a25ab74e75d907624bf831b5acd50a9f9d1cd113
Kyle Oman <kyle.a.oman@durham.ac.uk>
2025-04-15 17:56:47 +0100

    Remove ray_trace test (removed in swiftsimio).

--- tests/test_visualisation.py
+++ tests/test_visualisation.py
@@ -5,7 +5,6 @@ from swiftsimio import cosmo_array, cosmo_quantity
 from swiftsimio.visualisation.projection import project_gas
 from swiftsimio.visualisation.slice import slice_gas
 from swiftsimio.visualisation.volume_render import render_gas
-from swiftsimio.visualisation.ray_trace import panel_gas
 
 
 class TestRecenteredVisualisation:
@@ -114,43 +113,6 @@ class TestRecenteredVisualisation:
             sg,
             region=cosmo_array(
                 [-0.5, 0.5, -0.5, 0.5, -0.5, 0.5],
-                u.Mpc,
-                comoving=True,
-                scale_factor=1,
-                scale_exponent=1,
-            ),
-            **kwargs,
-        )
-        assert np.allclose(ref_img, recentered_img)
-
-    @pytest.mark.skip(reason="ray trace not documented (not fully implemented?)")
-    @pytest.mark.parametrize("z_slice", [False, True])
-    def test_recentered_ray_trace(self, sg, sg_autorecentre_off, z_slice):
-        """
-        We should be able to make the same ray trace whether we recentered or not.
-        """
-        z_limits = [1.5, 2.5] if z_slice else []
-        recentered_z_limits = [-0.5, 0.5] if z_slice else []
-        kwargs = {
-            "resolution": 4,
-            "panels": 4,
-            "project": "masses",
-        }
-        ref_img = panel_gas(
-            sg_autorecentre_off,
-            region=cosmo_array(
-                [1.5, 2.5, 1.5, 2.5] + z_limits,
-                u.Mpc,
-                comoving=True,
-                scale_factor=1,
-                scale_exponent=1,
-            ),
-            **kwargs,
-        )
-        recentered_img = panel_gas(
-            sg,
-            region=cosmo_array(
-                [-0.5, 0.5, -0.5, 0.5] + recentered_z_limits,
                 u.Mpc,
                 comoving=True,
                 scale_factor=1,
