6545264eb27f5600c3a81d2cd2f78f4886b9da2e
Kyle Oman <kyle.a.oman@durham.ac.uk>
2025-06-25 18:31:54 +0100

    Bump version for release and enfore version consistency.

---
+++ tests/test_version.py
@@ -0,0 +1,25 @@
+import importlib.metadata
+import swiftgalaxy
+
+
+class TestVersion:
+    """
+    Check that version numbering is consistent.
+    """
+
+    def test_code_version(self):
+        """
+        Check that code version matches pyproject.toml version.
+        """
+        assert importlib.metadata.version("swiftgalaxy") == swiftgalaxy.__version__
+
+    def test_codemeta_version(self):
+        """
+        Check that the version in codemeta.json matches pyproject.toml version.
+        """
+        with open("codemeta.json") as f:
+            codemeta_content = f.read()
+        assert (
+            f'"version": "{importlib.metadata.version("swiftgalaxy")}",'
+            in codemeta_content
+        )
