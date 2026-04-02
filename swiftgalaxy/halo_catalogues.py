Fix repeated masking (#90)
* Add regression test.

* Add argument to lazy mask so that it always evaluates on relevant SWIFTGalaxy.

* Bump version for release.

* Update some tests accordingly.

* Run formatter.

swiftgalaxy/halo_catalogues.py
@@ -798,9 +798,9 @@ class SOAP(_HaloCatalogue):
 
         Parameters
         ----------
-        sg : :class:`~swiftgalaxy.reader.SWIFTGalaxy`
-            The :class:`~swiftgalaxy.reader.SWIFTGalaxy` that this halo finder
-            interface is associated to.
+        sg : ~swiftgalaxy.reader.SWIFTGalaxy
+            The :class:`~swiftgalaxy.reader.SWIFTGalaxy` instance that this halo catalogue
+            is assoiated to.
 
         mask_loaded : :obj:`bool`
             Whether to mask any data loaded while creating the mask. The iterator wants to
@@ -834,7 +834,7 @@ class SOAP(_HaloCatalogue):
                 The generated function that evaluates a mask.
             """
 
-            def lazy_mask() -> NDArray:
+            def lazy_mask(active_sg: "SWIFTGalaxy") -> NDArray:
                 """
                 Evaluate a mask that selects bound particles.
 
@@ -843,20 +843,27 @@ class SOAP(_HaloCatalogue):
 
                 This function must mask the data (``group_nr_bound``) that it has loaded.
 
+                Parameters
+                ----------
+                active_sg : :class:`~swiftgalaxy.reader.SWIFTGalaxy`
+                    The :class:`~swiftgalaxy.reader.SWIFTGalaxy` instance being masked.
+
                 Returns
                 -------
                 :class:`~numpy.ndarray`
                     The mask that selects bound particles.
                 """
                 mask = getattr(
-                    sg, group_name
+                    active_sg, group_name
                 )._particle_dataset.group_nr_bound.to_value(
                     u.dimensionless
                 ) == self.input_halos.halo_catalogue_index.to_value(u.dimensionless)
                 if mask_loaded:
                     # mask the group_nr_bound array that we loaded
-                    getattr(sg, group_name)._particle_dataset._group_nr_bound = getattr(
-                        sg, group_name
+                    getattr(
+                        active_sg, group_name
+                    )._particle_dataset._group_nr_bound = getattr(
+                        active_sg, group_name
                     )._particle_dataset._group_nr_bound[mask]
                 return mask
 
@@ -1207,7 +1214,7 @@ class Velociraptor(_HaloCatalogue):
                 The generated function that evaluates a mask.
             """
 
-            def lazy_mask() -> NDArray:
+            def lazy_mask(active_sg: "SWIFTGalaxy") -> NDArray:
                 """
                 Evaluate a mask that selects bound particles.
 
@@ -1215,6 +1222,11 @@ class Velociraptor(_HaloCatalogue):
                 IDs.
 
                 This function must mask the data (``particle_ids``) that it has loaded.
+
+                Parameters
+                ----------
+                active_sg : :class:`~swiftgalaxy.reader.SWIFTGalaxy`
+                    The :class:`~swiftgalaxy.reader.SWIFTGalaxy` instance being masked.
 
                 Returns
                 -------
@@ -1233,7 +1245,7 @@ class Velociraptor(_HaloCatalogue):
                     else 1.0
                 )
                 mask = np.isin(
-                    getattr(sg, group_name)._particle_dataset.particle_ids,
+                    getattr(active_sg, group_name)._particle_dataset.particle_ids,
                     cosmo_array(
                         particles.particle_ids,
                         comoving=False,
@@ -1243,8 +1255,10 @@ class Velociraptor(_HaloCatalogue):
                 )
                 if mask_loaded:
                     # mask the particle_ids that we loaded
-                    getattr(sg, group_name)._particle_dataset._particle_ids = getattr(
-                        sg, group_name
+                    getattr(
+                        active_sg, group_name
+                    )._particle_dataset._particle_ids = getattr(
+                        active_sg, group_name
                     )._particle_dataset._particle_ids[mask]
                 return mask
 
@@ -1836,7 +1850,7 @@ class Caesar(_HaloCatalogue):
                 The generated function that evaluates a mask.
             """
 
-            def lazy_mask() -> Union[NDArray, slice]:
+            def lazy_mask(active_sg: "SWIFTGalaxy") -> Union[NDArray, slice]:
                 """
                 Evaluate a mask that selects bound particles.
 
@@ -1844,6 +1858,11 @@ class Caesar(_HaloCatalogue):
                 read in the spatial mask.
 
                 This function must mask the data that it has loaded, but it loads nothing.
+
+                Parameters
+                ----------
+                active_sg : :class:`~swiftgalaxy.reader.SWIFTGalaxy`
+                    The :class:`~swiftgalaxy.reader.SWIFTGalaxy` instance being masked.
 
                 Returns
                 -------
@@ -1854,13 +1873,15 @@ class Caesar(_HaloCatalogue):
                     return null_slice
                 mask = getattr(cat, list_name)
                 mask = mask[
-                    in_one_of_ranges(mask, getattr(sg._spatial_mask, group_name))
+                    in_one_of_ranges(mask, getattr(active_sg._spatial_mask, group_name))
                 ]
                 mask = np.isin(
                     np.concatenate(
                         [
                             np.arange(start, end)
-                            for start, end in getattr(sg._spatial_mask, group_name)
+                            for start, end in getattr(
+                                active_sg._spatial_mask, group_name
+                            )
                         ]
                     ),
                     mask,

tests/conftest.py
@@ -1590,9 +1590,14 @@ def lm() -> LazyMask:
         A lazy mask.
     """
 
-    def mf() -> np.ndarray:
+    def mf(arg: None) -> np.ndarray:
         """
         Create a simple mask function.
+
+        Parameters
+        ----------
+        arg : None
+            An argument is required, but for this test object its value is unused.
 
         Returns
         -------

tests/test_copy.py
@@ -167,7 +167,7 @@ class TestCopyMaskCollection:
             stars=None,
             black_holes=np.arange(3),
             lazy_evaluated=LazyMask(mask=np.ones(100, dtype=bool)),
-            lazy_unevaluated=LazyMask(mask_function=lambda: np.s_[:20]),
+            lazy_unevaluated=LazyMask(mask_function=lambda x: np.s_[:20]),
         )
         mc_copy = deepcopy(mc)
         assert set(mc_copy.__dict__.keys()) == set(mc.__dict__.keys())
@@ -177,8 +177,8 @@ class TestCopyMaskCollection:
                 assert comparison
             else:
                 assert all(comparison)
-        assert all(mc.lazy_evaluated.mask == mc_copy.lazy_evaluated.mask)
-        assert mc.lazy_unevaluated.mask == mc_copy.lazy_unevaluated.mask
+        assert all(mc.lazy_evaluated.mask(None) == mc_copy.lazy_evaluated.mask(None))
+        assert mc.lazy_unevaluated.mask(None) == mc_copy.lazy_unevaluated.mask(None)
 
 
 class TestCopyHaloCatalogue:

tests/test_halo_catalogues.py
@@ -188,11 +188,11 @@ class TestHaloCatalogues:
         for particle_type in _present_particle_types.values():
             if expected_shape[particle_type] is not None:
                 assert (
-                    getattr(generated_extra_mask, particle_type).mask.shape
+                    getattr(generated_extra_mask, particle_type).mask(sg).shape
                     == expected_shape[particle_type]
                 )
                 assert (
-                    getattr(generated_extra_mask, particle_type).mask.sum()
+                    getattr(generated_extra_mask, particle_type).mask(sg).sum()
                     == dict(
                         gas=_n_g_1,
                         dark_matter=_n_dm_1,
@@ -230,7 +230,7 @@ class TestHaloCatalogues:
                 )
             else:
                 assert (
-                    getattr(generated_extra_mask, particle_type).mask.sum()
+                    getattr(generated_extra_mask, particle_type).mask(sg).sum()
                     == dict(gas=100, dark_matter=None, stars=100, black_holes=_n_bh_1)[
                         particle_type
                     ]
@@ -418,11 +418,11 @@ class TestHaloCataloguesMulti:
         for particle_type in _present_particle_types.values():
             if expected_shape[particle_type] is not None:
                 assert (
-                    getattr(generated_extra_mask, particle_type).mask.shape
+                    getattr(generated_extra_mask, particle_type).mask(sg).shape
                     == expected_shape[particle_type]
                 )
                 assert (
-                    getattr(generated_extra_mask, particle_type).mask.sum()
+                    getattr(generated_extra_mask, particle_type).mask(sg).sum()
                     == dict(
                         gas=_n_g_1,
                         dark_matter=_n_dm_1,
@@ -654,8 +654,8 @@ class TestVelociraptorWithSWIFTGalaxy:
             )
             for ptype in _present_particle_types.values():
                 assert np.all(
-                    getattr(sg_from_sgs._extra_mask, ptype).mask
-                    == getattr(sg._extra_mask, ptype).mask
+                    getattr(sg_from_sgs._extra_mask, ptype).mask(sg_from_sgs)
+                    == getattr(sg._extra_mask, ptype).mask(sg)
                 )
 
     def test_lazy_masking_sg(self, sg_vr):
@@ -893,14 +893,13 @@ class TestCaesarWithSWIFTGalaxy:
             )
             for ptype in _present_particle_types.values():
                 if isinstance(getattr(sg_from_sgs._extra_mask, ptype), slice):
-                    assert (
-                        getattr(sg_from_sgs._extra_mask, ptype).mask
-                        == getattr(sg._extra_mask, ptype).mask
-                    )
+                    assert getattr(sg_from_sgs._extra_mask, ptype).mask(
+                        sg_from_sgs
+                    ) == getattr(sg._extra_mask, ptype).mask(sg)
                 else:
                     assert np.all(
-                        getattr(sg_from_sgs._extra_mask, ptype).mask
-                        == getattr(sg._extra_mask, ptype).mask
+                        getattr(sg_from_sgs._extra_mask, ptype).mask(sg_from_sgs)
+                        == getattr(sg._extra_mask, ptype).mask(sg)
                     )
 
     @pytest.mark.parametrize("group_type", ["halo", "galaxy"])
@@ -926,10 +925,10 @@ class TestCaesarWithSWIFTGalaxy:
                 toysnap["toysnap_filename"],
                 Caesar(toycaesar_filename, group_type=group_type, group_index=0),
             )
-            assert sg._extra_mask.gas.mask == np.s_[:0]
-            assert sg._extra_mask.stars.mask == np.s_[:0]
-            assert sg._extra_mask.dark_matter.mask == np.s_[:0]
-            assert sg._extra_mask.black_holes.mask == np.s_[:0]
+            assert sg._extra_mask.gas.mask(sg) == np.s_[:0]
+            assert sg._extra_mask.stars.mask(sg) == np.s_[:0]
+            assert sg._extra_mask.dark_matter.mask(sg) == np.s_[:0]
+            assert sg._extra_mask.black_holes.mask(sg) == np.s_[:0]
         finally:
             _remove_toycaesar(filename=toycaesar_filename)
 
@@ -1359,8 +1358,8 @@ class TestSOAPWithSWIFTGalaxy:
             )
             for ptype in _present_particle_types.values():
                 assert np.all(
-                    getattr(sg_from_sgs._extra_mask, ptype).mask
-                    == getattr(sg._extra_mask, ptype).mask
+                    getattr(sg_from_sgs._extra_mask, ptype).mask(sg_from_sgs)
+                    == getattr(sg._extra_mask, ptype).mask(sg)
                 )
 
     def test_lazy_masking_sg(self, sg_soap):

tests/test_masking.py
@@ -139,6 +139,60 @@ class TestMaskingSWIFTGalaxy:
         finally:
             _remove_toysnap(snapfile=toysnap_filename)
 
+    def test_repeated_copy_mask(self, sg_soap):
+        """
+        Check that we can apply a copying mask operation more than once.
+
+        Regression test for https://github.com/SWIFTSIM/swiftgalaxy/issues/89.
+        This had previously caused an ``IndexError``, specifically when using a
+        :class:`~swiftgalaxy.halo_catalogues.SOAP` catalogue (because it uses a boolean
+        ``"bound_only"`` mask).
+        """
+        sg_copy1 = sg_soap[
+            MaskCollection(
+                gas=sg_soap.gas.spherical_coordinates.r
+                < cosmo_quantity(
+                    3,
+                    u.kpc,
+                    comoving=True,
+                    scale_factor=sg_soap.metadata.scale_factor,
+                    scale_exponent=1,
+                )
+            )
+        ]
+        sg_copy2 = sg_soap[
+            MaskCollection(
+                gas=sg_soap.gas.spherical_coordinates.r
+                < cosmo_quantity(
+                    2,
+                    u.kpc,
+                    comoving=True,
+                    scale_factor=sg_soap.metadata.scale_factor,
+                    scale_exponent=1,
+                )
+            )
+        ]
+        assert (
+            sg_copy1.gas.spherical_coordinates.r
+            < cosmo_quantity(
+                3,
+                u.kpc,
+                comoving=True,
+                scale_factor=sg_soap.metadata.scale_factor,
+                scale_exponent=1,
+            )
+        ).all()
+        assert (
+            sg_copy2.gas.spherical_coordinates.r
+            < cosmo_quantity(
+                2,
+                u.kpc,
+                comoving=True,
+                scale_factor=sg_soap.metadata.scale_factor,
+                scale_exponent=1,
+            )
+        ).all()
+
 
 class TestMaskingParticleDatasets:
     """Test applying masks to particle datasets."""
@@ -337,25 +391,25 @@ class TestLazyMask:
         """Check that accessing mask triggers evaluation if lazy."""
         assert lm._evaluated is False
         assert not hasattr(lm, "_mask")
-        assert (lm.mask == lm._mask_function()).all()
+        assert (lm.mask(None) == lm._mask_function(None)).all()
         assert lm._evaluated
-        assert (lm._mask == lm._mask_function()).all()
+        assert (lm._mask == lm._mask_function(None)).all()
 
     def test_access_not_lazy(self):
         """Check that accessing the mask works for a non-lazy mask."""
         m = np.ones(10, dtype=bool)
         lm = LazyMask(mask=m)
         assert lm._evaluated
-        assert (lm.mask == m).all()
+        assert (lm.mask(None) == m).all()
 
     def test_manual_trigger_eval(self, lm):
         """Check that accessing mask triggers evaluation if lazy."""
         assert lm._evaluated is False
         assert not hasattr(lm, "_mask")
-        lm._evaluate()
+        lm._evaluate(None)
         assert lm._evaluated
-        assert (lm.mask == lm._mask_function()).all()
-        assert (lm._mask == lm._mask_function()).all()
+        assert (lm.mask(None) == lm._mask_function(None)).all()
+        assert (lm._mask == lm._mask_function(None)).all()
 
     def test_trigger_eval_once_only(self):
         """Check that we can't trigger mask evaluation repeatedly."""
@@ -365,9 +419,14 @@ class TestLazyMask:
 
             call_counter: int = 0
 
-            def __call__(self):
+            def __call__(self, arg: None):
                 """
                 Call the class to behave like a simple mask function.
+
+                Parameters
+                ----------
+                arg : None
+                    An argument is required but its value is unused.
 
                 Returns
                 -------
@@ -381,12 +440,12 @@ class TestLazyMask:
         lm = LazyMask(mask_function=mf)
         assert lm._evaluated is False
         # trigger a mask evaluation:
-        lm.mask
+        lm.mask(None)
         assert lm._evaluated is True
         assert mf.call_counter == 1
         # we shouldn't be able to trigger or force another mask evaluation:
-        lm.mask
-        lm._evaluate()
+        lm.mask(None)
+        lm._evaluate(None)
         assert mf.call_counter == 1
 
     def test_copy(self, lm):
@@ -397,12 +456,12 @@ class TestLazyMask:
         assert not hasattr(lm_unevaluated_copy, "_mask")
         assert lm_unevaluated_copy._mask_function is lm._mask_function
         # trigger evaluated
-        lm._evaluate()
+        lm._evaluate(None)
         assert lm._evaluated
         # now copy after evaluating
         lm_evaluated_copy = copy(lm)
         assert lm_evaluated_copy._evaluated
-        assert (lm_evaluated_copy._mask == lm._mask_function()).all()
+        assert (lm_evaluated_copy._mask == lm._mask_function(None)).all()
         assert lm_evaluated_copy._mask_function is lm._mask_function
         # and test a non-lazy mask
         m = np.ones(10, dtype=bool)
@@ -420,12 +479,12 @@ class TestLazyMask:
         assert not hasattr(lm_unevaluated_copy, "_mask")
         assert lm_unevaluated_copy._mask_function is lm._mask_function
         # trigger evaluated
-        lm._evaluate()
+        lm._evaluate(None)
         assert lm._evaluated
         # now copy after evaluating
         lm_evaluated_copy = deepcopy(lm)
         assert lm_evaluated_copy._evaluated
-        assert (lm_evaluated_copy._mask == lm._mask_function()).all()
+        assert (lm_evaluated_copy._mask == lm._mask_function(None)).all()
         assert lm_evaluated_copy._mask_function is lm._mask_function
         # and test a non-lazy mask
         m = np.ones(10, dtype=bool)
@@ -442,8 +501,8 @@ class TestLazyMask:
             ValueError, match="Cannot compare when one or more masks are not evaluated."
         ):
             lm == lm2
-        lm._evaluate()
-        lm2._evaluate()
+        lm._evaluate(None)
+        lm2._evaluate(None)
         assert lm == lm2
         lmn = LazyMask(mask=None)
         assert lm != lmn
@@ -455,7 +514,7 @@ class TestLazyMask:
             ValueError, match="Cannot compare when one or more masks are not evaluated."
         ):
             lm == m
-        lm._evaluate()
+        lm._evaluate(None)
         assert lm == m
         assert lm != np.zeros(10, dtype=bool)
 